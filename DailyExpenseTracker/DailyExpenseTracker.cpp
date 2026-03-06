79a782d2764f18a77b051491d1a955611776dfcd
HEER SHAH <heershah.ce@gmail.com>
2026-03-06 15:26:54 +0530

    Add files via upload
    This code will keep record of daily expenses and give total expense

---
+++ DailyExpenseTracker/DailyExpenseTracker.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<fstream>
+#include<string>
+using namespace std;
+int main()
+{
+    fstream file("DET.txt",ios::app| ios::in | ios::out);
+    float ex,total=0;
+    if(file.is_open())
+    {
+        cout<<"Add Expense:";
+        cin>>ex;
+        file<<ex<<endl;
+    }
+    file.seekg(0);
+
+    while(file>>ex)
+    {
+        cout<<ex<<endl;
+        total += ex;
+    }
+    cout<<"Total expense:"<<total;
+    file.close();
+
+}
